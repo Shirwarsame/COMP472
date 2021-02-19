e968a8131ca71f89b6f3a79d437e2d722efa17c2
Jeff Buonamici <jeffbuonamici@users.noreply.github.com>
2021-02-18 22:07:09 -0500

    Code clean up

--- SOEN472_A1.py
+++ SOEN472_A1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 from codecs import open
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -7,28 +8,24 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import metrics
 
+vectorizer = CountVectorizer()
 
+# Read file
 def read_documents(doc_file):
     docs = []
     labels = []
-    with open(doc_file, encoding='utf-8') as f:
-        for line in f:
-            words = line.strip().split()
-            docs.append(words[3:])
-            labels.append(words[1])
+    try:
+        with open(doc_file, encoding='utf-8') as f:
+            for line in f:
+                words = line.strip().split()
+                docs.append(words[3:])
+                labels.append(words[1])
+    except FileNotFoundError:
+        print("File does not exist!")
+        sys.exit()
     return docs, labels
 
-all_docs, all_labels = read_documents('all_sentiment_shuffled.txt')
-
-# Transforming a list of list into a list of Strings
-all_docs = [' '.join(ele) for ele in all_docs]
-
-split_point = int(0.75*len(all_docs))
-train_docs = all_docs[:split_point]
-train_labels = all_labels[:split_point]
-eval_docs = all_docs[split_point:]
-eval_labels = all_labels[split_point:]
-
+# Plotting for Bar Graph
 def plot_bar(labels):
     unique_labels = sorted(list(set(labels)))
     counts = [labels.count(label) for label in unique_labels]
@@ -42,35 +39,7 @@ def plot_bar(labels):
     plt.ylabel("Frequency")
     plt.show()
 
-
-def plot_pie(labels):
-    unique_labels = sorted(list(set(labels)))
-    counts = [labels.count(label) for label in unique_labels]
-    total = sum(counts)
-    sizes = [count / total for count in counts]
-
-    fig1, ax1 = plt.subplots()
-    ax1.pie(sizes, labels=unique_labels, autopct='%1.1f%%', startangle=90)
-    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-    plt.show()
-
-plot_bar(all_labels)
-
-#==============================================================================
-#================================TODO Section==================================
-#==============================================================================
-# TODO:Ploting (task 1)
-# TODO:Adding functions as shown in the appendex
-# - def score_doc_label(document, label, classifier):
-# - All the functions needed for the 2 decision tree models
-
-#==============================================================================
-#Some methods extracted from the previous code to fit the appendex construction
-#==============================================================================
-
-vectorizer = CountVectorizer()
-
+# Train Naive Bayes
 def train_nb(documents, labels):
     # This function trains the Naive-Bayes model with training data (documents and lables)
     # and returns a classifier. The instance of the NB used is the multinominal.
@@ -82,34 +51,7 @@ def train_nb(documents, labels):
     clf_nb = MultinomialNB().fit(X_train_tfidf, labels)
     return(clf_nb)
 
-def score_doc_label(document, label, classifier):
-    #TODO
-    return #Probability of getting those words for a specfic label - see appendex
-
-def classify_nb(document, classifier):
-    # This function predicts the label of a document based on the classifier passed
-    X_eval_counts = vectorizer.transform(document)
-    predicted = classifier.predict(X_eval_counts)
-    return predicted
-
-def accuracy(true_labels, guessed_labels):
-    # This function evaluates the accuracy of a classifier by compated the true labels to the guessed labels
-    evaluation = np.mean(true_labels == guessed_labels)
-    metric = metrics.classification_report(eval_labels, guessed_labels,list(set(eval_labels)))
-    return evaluation, metric
-
-# Example of the usage of the functions defined above (driver example)
-clf_nb1 = train_nb(train_docs, train_labels)
-print(clf_nb1)
-
-predicted_nb1 = classify_nb(eval_docs, clf_nb1)
-print(predicted_nb1)
-
-eval1, metric1 = accuracy(eval_labels, predicted_nb1)
-print(eval1)
-print(metric1)
-
-# Base Decision Tree ML algo
+# Train Base Decision Tree
 def train_base_DT(documents, labels):
     vectorizer = CountVectorizer()
     X_train_counts = vectorizer.fit_transform(documents)
@@ -120,6 +62,7 @@ def train_base_DT(documents, labels):
     model = DecisionTreeClassifier(criterion='entropy').fit(X_train_tfidf, labels)
     return model
 
+# Train Best Decision Tree
 def train_best_DT(documents, labels):
     vectorizer = CountVectorizer()
     X_train_counts = vectorizer.fit_transform(documents)
@@ -130,9 +73,21 @@ def train_best_DT(documents, labels):
     model = DecisionTreeClassifier().fit(X_train_tfidf, labels)
     return model
 
+def classify(document, classifier):
+    # This function predicts the label of a document based on the classifier passed
+    X_eval_counts = vectorizer.transform(document)
+    predicted = classifier.predict(X_eval_counts)
+    return predicted
+
+def accuracy(true_labels, guessed_labels, eval_labels):
+    # This function evaluates the accuracy of a classifier by compated the true labels to the guessed labels
+    evaluation = np.mean(true_labels == guessed_labels)
+    metric = metrics.classification_report(eval_labels, guessed_labels,list(set(eval_labels)))
+    return evaluation, metric
+
 def report(filename, eval_labels, prediction):
     f = open(filename + ".txt", "w")
-    eval1, metric1 = accuracy(eval_labels, prediction)
+    eval1, metric1 = accuracy(eval_labels, prediction, eval_labels)
     f.write('---------------\n' + filename +'\n' + '---------------\n\n')
     f.write('---------------\n' + 'Accuracy\n' + '---------------\n')
     f.write(str(eval1) + '\n\n')
@@ -152,13 +107,40 @@ def report(filename, eval_labels, prediction):
         f.write('\n')
     f.close()
 
-print(predicted_nb1)
-report("NaiveBayes_all_sentiment_shuffled", eval_labels, predicted_nb1)
+def main(filename):
+    # (Task 0)
+    all_docs, all_labels = read_documents(filename + '.txt')
 
-clf_base_dt = train_base_DT(train_docs, train_labels)
-predicted_base_dt = classify_nb(eval_docs, clf_base_dt)
-report("Base_DT_all_sentiment_shuffled", eval_labels, predicted_base_dt)
+    # Transforming a list of list into a list of Strings
+    all_docs = [' '.join(ele) for ele in all_docs]
 
-clf_best_dt = train_best_DT(train_docs, train_labels)
-predicted_best_dt = classify_nb(eval_docs, clf_best_dt)
-report("Best_DT_all_sentiment_shuffled", eval_labels, predicted_best_dt)
+    split_point = int(0.80*len(all_docs))
+    train_docs = all_docs[:split_point]
+    train_labels = all_labels[:split_point]
+    eval_docs = all_docs[split_point:]
+    eval_labels = all_labels[split_point:]
+
+    # Plot on Bar Graph (Task 1)
+    plot_bar(all_labels)
+
+    # (Task 2 & 3)
+    # Naive Bayes
+    clf_nb1 = train_nb(train_docs, train_labels)
+    predicted_nb1 = classify(eval_docs, clf_nb1)
+    report("NaiveBayes_" + filename, eval_labels, predicted_nb1)
+
+    # Base Decision Tree
+    clf_base_dt = train_base_DT(train_docs, train_labels)
+    predicted_base_dt = classify(eval_docs, clf_base_dt)
+    report("Base_DT_" + filename, eval_labels, predicted_base_dt)
+
+    # Best Decision Tree
+    clf_best_dt = train_best_DT(train_docs, train_labels)
+    predicted_best_dt = classify(eval_docs, clf_best_dt)
+    report("Best_DT_" + filename, eval_labels, predicted_best_dt)
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
+    else:
+        print("Enter a single file name!")
