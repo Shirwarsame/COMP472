0448465b89049de8d496d0fe2244364c89b7654f
Paul Micu <35611221+PaulMicu@users.noreply.github.com>
2021-02-18 22:15:54 -0500

    some code cleanup
    just some renames and spacing

--- SOEN472_A1.py
+++ SOEN472_A1.py
@@ -10,6 +10,7 @@ from sklearn import metrics
 
 vectorizer = CountVectorizer()
 
+
 # Read file
 def read_documents(doc_file):
     docs = []
@@ -25,6 +26,7 @@ def read_documents(doc_file):
         sys.exit()
     return docs, labels
 
+
 # Plotting for Bar Graph
 def plot_bar(labels):
     unique_labels = sorted(list(set(labels)))
@@ -39,56 +41,62 @@ def plot_bar(labels):
     plt.ylabel("Frequency")
     plt.show()
 
+
 # Train Naive Bayes
 def train_nb(documents, labels):
-    # This function trains the Naive-Bayes model with training data (documents and lables)
-    # and returns a classifier. The instance of the NB used is the multinominal.
-    X_train_counts = vectorizer.fit_transform(documents)
+    # This function trains the Naive-Bayes model with training data (documents and labels)
+    # and returns a classifier. The instance of the NB used is the multinomial.
+    x_train_counts = vectorizer.fit_transform(documents)
 
     tfidf_transformer = TfidfTransformer()
-    X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+    x_train_tfidf = tfidf_transformer.fit_transform(x_train_counts)
 
-    clf_nb = MultinomialNB().fit(X_train_tfidf, labels)
-    return(clf_nb)
+    clf_nb = MultinomialNB().fit(x_train_tfidf, labels)
+    return clf_nb
+
 
 # Train Base Decision Tree
-def train_base_DT(documents, labels):
+def train_base_dt(documents, labels):
     vectorizer = CountVectorizer()
-    X_train_counts = vectorizer.fit_transform(documents)
+    x_train_counts = vectorizer.fit_transform(documents)
 
     tfidf_transformer = TfidfTransformer()
-    X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+    x_train_tfidf = tfidf_transformer.fit_transform(x_train_counts)
 
-    model = DecisionTreeClassifier(criterion='entropy').fit(X_train_tfidf, labels)
+    model = DecisionTreeClassifier(criterion='entropy').fit(x_train_tfidf, labels)
     return model
+
 
 # Train Best Decision Tree
-def train_best_DT(documents, labels):
+def train_best_dt(documents, labels):
     vectorizer = CountVectorizer()
-    X_train_counts = vectorizer.fit_transform(documents)
+    x_train_counts = vectorizer.fit_transform(documents)
 
     tfidf_transformer = TfidfTransformer()
-    X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+    x_train_tfidf = tfidf_transformer.fit_transform(x_train_counts)
 
-    model = DecisionTreeClassifier().fit(X_train_tfidf, labels)
+    model = DecisionTreeClassifier().fit(x_train_tfidf, labels)
     return model
+
 
 def classify(document, classifier):
     # This function predicts the label of a document based on the classifier passed
-    X_eval_counts = vectorizer.transform(document)
-    predicted = classifier.predict(X_eval_counts)
+    x_eval_counts = vectorizer.transform(document)
+    predicted = classifier.predict(x_eval_counts)
     return predicted
 
+
 def accuracy(true_labels, guessed_labels, eval_labels):
-    # This function evaluates the accuracy of a classifier by compated the true labels to the guessed labels
+    # This function evaluates the accuracy of a classifier by competed the true labels to the guessed labels
     evaluation = np.mean(true_labels == guessed_labels)
-    metric = metrics.classification_report(eval_labels, guessed_labels,list(set(eval_labels)))
+    metric = metrics.classification_report(eval_labels, guessed_labels, list(set(eval_labels)))
     return evaluation, metric
+
 
 def report(filename, eval_labels, prediction):
     f = open(filename + ".txt", "w")
     eval1, metric1 = accuracy(eval_labels, prediction, eval_labels)
-    f.write('---------------\n' + filename +'\n' + '---------------\n\n')
+    f.write('---------------\n' + filename + '\n' + '---------------\n\n')
     f.write('---------------\n' + 'Accuracy\n' + '---------------\n')
     f.write(str(eval1) + '\n\n')
     f.write('---------------\n' + 'Performance Analysis\n' + '---------------\n')
@@ -107,6 +115,7 @@ def report(filename, eval_labels, prediction):
         f.write('\n')
     f.close()
 
+
 def main(filename):
     # (Task 0)
     all_docs, all_labels = read_documents(filename + '.txt')
@@ -114,7 +123,7 @@ def main(filename):
     # Transforming a list of list into a list of Strings
     all_docs = [' '.join(ele) for ele in all_docs]
 
-    split_point = int(0.80*len(all_docs))
+    split_point = int(0.80 * len(all_docs))
     train_docs = all_docs[:split_point]
     train_labels = all_labels[:split_point]
     eval_docs = all_docs[split_point:]
@@ -130,14 +139,15 @@ def main(filename):
     report("NaiveBayes_" + filename, eval_labels, predicted_nb1)
 
     # Base Decision Tree
-    clf_base_dt = train_base_DT(train_docs, train_labels)
+    clf_base_dt = train_base_dt(train_docs, train_labels)
     predicted_base_dt = classify(eval_docs, clf_base_dt)
     report("Base_DT_" + filename, eval_labels, predicted_base_dt)
 
     # Best Decision Tree
-    clf_best_dt = train_best_DT(train_docs, train_labels)
+    clf_best_dt = train_best_dt(train_docs, train_labels)
     predicted_best_dt = classify(eval_docs, clf_best_dt)
     report("Best_DT_" + filename, eval_labels, predicted_best_dt)
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
